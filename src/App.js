63a85420476b6654cbfe633e89ff9d195b6840e1
Dario Turin <dturin1997@gmail.com>
2022-06-07 18:07:10 -0500

    Segundo commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Users from './users'
 import Contact from './contact'
 import Home from './home'
 import Notfound from './notfound';
+import Producto from './productos';
+import ProductoDetalle from './productosDetalle';
 
 import { BrowserRouter as Router,Link, Route, Routes} from 'react-router-dom'
 
@@ -25,11 +27,16 @@ class App extends React.Component {
             <li>
               <Link to="/contacto">Contact</Link>
             </li>
+            <li>
+              <Link to="/productos">Productos</Link>
+            </li>
           </ul>
 
           <Routes>
             
               <Route path="/" element={<Home/>} />
+              <Route path="/productos" element={<Producto/>} />
+              <Route path="/productos/:id" element={<ProductoDetalle/>} />
               
               <Route path="/usuarios/:id" element={<Users/>}></Route>
               
